9c4c1ce88fc6a339a704f3c4ee1a34487b4999d7
Samuel-RM <samiriverame07@gmail.com>
2022-11-29 00:26:29 -0600

    Actualizando proyecto

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get():
+    return [1,2,3,4,5,]
+ 
+
+@app.get('/contact', response_class=HTMLResponse)
+def get():
+    return """
+        <h1>Hola Mundo</h1>
+        <P>Hola soy una pagina creada por un weon</p>
+    """
+
+
 
 def run():
     store.get_categories()
